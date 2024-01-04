a5bcb577edbabd933a1791e06924c996b39d2f91
Godfreyoz <ajeyemi.godfrey@gmail.com>
2024-01-04 16:54:21 +0100

    chatbot

--- blog/views.py
+++ blog/views.py
@@ -3,12 +3,34 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+bot = ChatBot('chatbot', read_only=False, logic_adapters=['chatterbot.logic.BestMatch'])
+
+list_to_train = [
+
+     "hi",
+     "hi, there",
+     "what's your name",
+     "I'm just a chatbot",
+     "what is your fav food",
+     "I like cheese",
+]
+
+
+
+list_trainer = ListTrainer(bot)
+list_trainer.train(list_to_train)
+
+
 
 def index(requset):
     return render(requset, 'blog/index.html')
 
 def specific(requset):
     return HttpResponse("list1")
+
 
 def getResponse(requset):
     userMessage = requset.GET.get('userMessage')
