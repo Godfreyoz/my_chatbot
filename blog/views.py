2d497001df59f238f4d246e83cedf1dbcd2329c7
Godfreyoz <ajeyemi.godfrey@gmail.com>
2024-01-08 17:04:49 +0100

    chatbot

--- blog/views.py
+++ blog/views.py
@@ -4,24 +4,43 @@ from django.http import HttpResponse
 # Create your views here.
 
 from chatterbot import ChatBot
-from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer
 
-bot = ChatBot('chatbot', read_only=False, logic_adapters=['chatterbot.logic.BestMatch'])
+bot = ChatBot('chatbot', read_only=False,
+              logic_adapters=[
+                  {
+                
+                      'import_path':'chatterbot.logic.BestMatch',
+                      'default_response':'Sorry, out of my reach',
+                      'maximun_similarity_threshold': 100
+                     
+                      }
+                      ])
 
 list_to_train = [
 
      "hi",
      "hi, there",
-     "what's your name",
+     "what's your name?",
      "I'm just a chatbot",
-     "what is your fav food",
-     "I like cheese",
+     "what is your fav food?",
+     "I like yam and egg",
+     "how are you doing today?",
+     "I'm doing great Thanks",
+     "what's your fav sport?",
+     "football",
+     "do you have children?",
+     "no",
 ]
+
+
+#chatterbotCorpusTainer = ChatterBotCorpusTrainer(bot)
 
 
 
 list_trainer = ListTrainer(bot)
 list_trainer.train(list_to_train)
+#chatterbotCorpusTainer.train('chatterbot.corpus.english')
 
 
 
@@ -34,4 +53,5 @@ def specific(requset):
 
 def getResponse(requset):
     userMessage = requset.GET.get('userMessage')
-    return HttpResponse(userMessage)
+    chatResponse = str (bot.get_response(userMessage))
+    return HttpResponse(chatResponse)
